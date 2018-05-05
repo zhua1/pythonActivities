a4ba4a0c7c545d073b746faf9f79bbfb6759f01a
unknown <mika-hua@hotmail.com>
2018-05-05 12:49:06 -0700

    activity4 --- employee emails

--- activity4.py
+++ activity4.py
@@ -3,22 +3,6 @@ p = 'C:\\Users\\meifl\\Desktop\\UCIRV201804DATA3-Class-Repository-DATA\\01-Lesso
 
 import csv
 
-file1 = csv.DictReader(open(p))
-
-new_employee_data = []
-
-for row in file1:
-    new_employee_data.append(row['First Name'] + '.' + row['Last Name'] + '@example.com')
-
-
-
-
-
-
-
-
-
-
 with open('C:\\Users\\meifl\\Desktop\\UCIRV201804DATA3-Class-Repository-DATA\\01-LessonPlan\\03-Python\\3\\Activities\\Unsolved\\04-Stu_Email\\output\\employee emails.csv', 'w', newline='') as csvfile:
     file1 = csv.DictReader(open(p))
     fieldnames = ['First Name', 'Last Name', 'SSN', 'Emails']
@@ -27,3 +11,18 @@ with open('C:\\Users\\meifl\\Desktop\\UCIRV201804DATA3-Class-Repository-DATA\\01
     for row in file1:
         writer.writerow({'First Name': row['First Name'], 'Last Name': row['Last Name'], 'SSN': row['SSN'], 'Emails': (row['First Name'] + '.' + row['Last Name'] + '@example.com')})
     
+    
+    
+    
+    
+    
+    
+# =============================================================================
+# file1 = csv.DictReader(open(p))
+# 
+# new_employee_data = []
+# 
+# for row in file1:
+#     new_employee_data.append(row['First Name'] + '.' + row['Last Name'] + '@example.com')
+# 
+# =============================================================================
